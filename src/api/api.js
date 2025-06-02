7d092b4fdd103a0ef9b35647580558c26d5b9cc4
Piyush Shinde <workwithpiyush77@gmail.com>
2025-06-02 12:25:45 +0530

    fixed catch block

--- src/api/api.js
+++ src/api/api.js
@@ -5,10 +5,11 @@ export const BACKEND_ENDPOINT = "https://xcountries-backend.azurewebsites.net/al
 export const fetchCountries = async () => {
     try{ 
         const res = await axios.get(BACKEND_ENDPOINT);
-        console.log(res.data);
+        // console.log(res.data);
         return res.data;
     }
     catch(e){
-        console.error(e);
+         console.error("API fetch failed:", e);
+        return []; 
     }
 }
